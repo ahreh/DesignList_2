9e622cd0e3ddbfdcedeaf8030d03e7eb01b25005
mokdo <tjdahreh@naver.com>
2017-06-07 19:50:43 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20120675/designlist/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20120675.designlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.room_cover);
+    }
+}
